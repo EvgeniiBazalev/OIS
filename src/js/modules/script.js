79e66ffb3f7f4a4a99ac906ffaf8f1d97fb11e8f
Evgenii Bazalev <bazalevj@gmail.com>
2022-10-17 14:19:09 +0500

    test commit

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -6,7 +6,6 @@
 //     console.log('Hi Dear')
 // // }
 
-<<<<<<< HEAD
 const arr = [1, 3, 5, 7, 9, 11];
 
 let secondArr = [2, 4, 6, 8, 10, 12];
@@ -18,6 +17,8 @@ function compareNum (a, b) {
     return a-b;
 }
 
+
+
 // arr[20] = 25;
 
 // // for (let i=0; i<arr.length; i++) {
@@ -27,56 +28,3 @@ function compareNum (a, b) {
 // arr.forEach(function(element, i, arr) {
 //     console.log(`${i}: ${element} внутри массива ${arr}`);
 // });
-=======
-// //const { LoaderTargetPlugin } = require("webpack");
-
-// // hiDear()
-
-
-// let obj = {
-//     name: 'Jenia',
-//     lastname: 'Bazalev',
-//     family: {
-//         wife: 'Liza',
-//         dother1: 'Lera',
-//         dother2: 'Olka',
-//         numberOfHouse: 19
-//     }
-// };
-
-
-// for(var i=0; i<10; i++) {
-//     console.log(i);
-//   }
-
-// // setTimeout(function() {
-// //   name = "Петя";
-// //   sayHi();
-// // }, 1000);
-
-
-// let arr = [1, 2, 3];
-// arr.something = 5;
-// arr[20]=55;
-
-// let i;
-
-// for (i in arr) {
-//     console.log(`${i} соответсвует ${arr[i]}`);
-// };
-
-// console.log(arr.length);
-
-// let newArr = {...arr};
-// console.log(newArr);
-
-// //  alert('Privet!')
-//  alert('Privet!');
-// [1,2].forEach(alert);
-
-// let result = prompt('Введите что-нибудь', 'Уже заполнено');
-// let result = confirm("Ты тут главный");
-// alert(result);
-// let a = NaN;
-console.log(null == "\n0\n");
->>>>>>> 79812f6e25b3c301051933245037724d46e01c0b
