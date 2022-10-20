8e92b760119bcad4698322dc7a6650f0e0c8e117
Evgenii <bazalevj@gmail.com>
2022-10-20 17:44:40 +0500

    js

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -6,22 +6,26 @@
 //     console.log('Hi Dear')
 // // }
 
-const arr = [1, 3, 5, 7, 9, 11];
+// const arr = [1, 3, 5, 7, 9, 11];
 
-let secondArr = [2, 4, 6, 8, 10, 12];
-// arr[20]=25;
-
-
+// let secondArr = [2, 4, 6, 8, 10, 12];
+// // arr[20]=25;
 
 
 
-console.log(arr.sort(compareNum));
-
-function compareNum (a, b) {
-    return a-b;
-}
 
 
+// console.log(arr.sort(compareNum));
+
+// function compareNum (a, b) {
+//     return a-b;
+// }
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+// a = +a;
+// b = +b;
+// alert(a + b); // 3
 
 
 
