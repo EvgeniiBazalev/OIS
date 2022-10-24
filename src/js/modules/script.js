41f1aa5807c93d8e29e4901184821e9f08da5404
Evgenii <bazalevj@gmail.com>
2022-10-24 17:54:13 +0500

    js***

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-let answer = Number(prompt('введите число', 'введите сюда'));
+// let answer = Number(prompt('введите число', 'введите сюда'));
 
 
-    (answer>0) ? alert(1):
-    (answer<0) ? alert(-1):
-    (answer==0) ? alert('Nol'): alert('ne opoznano');
+//     (answer>0) ? alert(1):
+//     (answer<0) ? alert(-1):
+//     (answer==0) ? alert('Nol'): alert('ne opoznano');
 
 
 
@@ -19,7 +19,23 @@ function compareNum (a, b) {
     return a-b;
 }
 
+let result;
 
+let a = 1;
+let b = 'null';
+
+(a + b < 4) ? result = 'malo' :
+(a + b > 4) ? result = 'mnogo' :
+(a + b > 4) ? result = 'ravno': result = 'hz';
+console.log(result);
+
+let message;
+let login = '';
+(login == 'Сотрудник') ? message = 'Привет':
+(login == 'Директор') ? message = 'Здравствуйте':
+(login == '') ? message = 'Нет логина': message = '';
+
+console.log(message);
 
 
 
