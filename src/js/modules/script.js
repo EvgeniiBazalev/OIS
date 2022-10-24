df08c8babbef6499716c0dfc3d50311d8d551faa
Evgenii <bazalevj@gmail.com>
2022-10-24 08:06:35 +0500

    js*

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,42 +1,17 @@
 'use strict';
 
+let answer = Number(prompt('введите число', 'введите сюда'));
 
 
-// export function hiDear() {
-//     console.log('Hi Dear')
-// // }
-
-// const arr = [1, 3, 5, 7, 9, 11];
-
-// let secondArr = [2, 4, 6, 8, 10, 12];
-// // arr[20]=25;
+    (answer>0) ? alert(1):
+    (answer<0) ? alert(-1):
+    (answer==0) ? alert('Nol'): alert('ne opoznano');
 
 
 
-
-
-// console.log(arr.sort(compareNum));
-
-// function compareNum (a, b) {
-//     return a-b;
+// if (answer == 'ECMAScript') {
+//     alert('Ok');
+// } else {
+//     alert('Ne verno');
 // }
 
-// let a = prompt("Первое число?", 1);
-// let b = prompt("Второе число?", 2);
-// a = +a;
-// b = +b;
-// alert(a + b); // 3
-
-
-
-
-
-// arr[20] = 25;
-
-// // for (let i=0; i<arr.length; i++) {
-// // console.log(arr[i]);
-// // }
-
-// arr.forEach(function(element, i, arr) {
-//     console.log(`${i}: ${element} внутри массива ${arr}`);
-// });
