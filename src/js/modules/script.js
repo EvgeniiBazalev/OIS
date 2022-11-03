10de3e80f7d8f18dafbe3f7023376a7eb1302ede
Evgenii <bazalevj@gmail.com>
2022-11-03 16:16:37 +0500

    js****

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,52 +1,80 @@
 'use strict';
 
-// let answer = Number(prompt('введите число', 'введите сюда'));
+// let user = prompt('введите логин', '');
+// let password;
+// // alert(user);
 
+// if (user == 'Админ') {
+//     password = prompt('введите пароль', '');
 
-//     (answer>0) ? alert(1):
-//     (answer<0) ? alert(-1):
-//     (answer==0) ? alert('Nol'): alert('ne opoznano');
-
-
-
-// if (answer == 'ECMAScript') {
-//     alert('Ok');
+//     if (password == 'Я Главный') {
+//         alert('Здравствуй Админ');
+//     } else if (password == null) {
+//         alert('Отменено');
+//     } else if (password != 'Я Главный') {
+//         alert('Неверный пароль');    
+//     } else {
+//         alert('Не понятно, что произошло');
+//     }
+// } else if (user == null) {
+//     alert('Отменено');
+// } else if (user != 'Админ') {
+//     alert('Ты не Админ!');
 // } else {
-//     alert('Ne verno');
-// }
-
-function compareNum (a, b) {
-    return a-b;
-}
-
-let result;
-
-let a = 1;
-let b = 'null';
-
-(a + b < 4) ? result = 'malo' :
-(a + b > 4) ? result = 'mnogo' :
-(a + b > 4) ? result = 'ravno': result = 'hz';
-console.log(result);
-
-let message;
-let login = '';
-(login == 'Сотрудник') ? message = 'Привет':
-(login == 'Директор') ? message = 'Здравствуйте':
-(login == '') ? message = 'Нет логина': message = '';
-
-console.log(message);
-
-
-
-
-
-// arr[20] = 25;
-
-// // for (let i=0; i<arr.length; i++) {
-// // console.log(arr[i]);
+//     alert('Не понятно, что произошло');
 // // }
 
-// arr.forEach(function(element, i, arr) {
-//     console.log(`${i}: ${element} внутри массива ${arr}`);
-// });
+// for (let i = 0; i < 3; ++i) {
+
+//     console.log(`this is number ${i}!`);
+
+// }
+// let i = 0;
+
+// while (++i<3) {
+//     console.log(`this is number ${i}!`);
+// }
+
+// let result = 99;
+
+// while (result < 100) {
+//     result = prompt('Введите число больше 100', 'сюда');
+    
+
+//     if (result == null) {
+//         break;
+//     } else if (!Number(result)) {
+//         result = 5;
+//     }
+// }
+
+// let a = 'Привет';
+// a = Number(a);
+// console.log(a);
+
+// console.log(a == NaN);
+
+// let count = 0;
+// let maxNumber = 55;
+
+// let i;
+// let j;
+
+// for (i=1; i<maxNumber; i++){
+
+//     for (j=2; j < maxNumber; j++){
+
+//         // console.log(`i равна ${i}, j равна ${j}`)
+//         // console.log(`остаток от деления равен ${i%j}`)
+
+//         if (i%j == 0) {
+//             count++;
+//         }
+//         // console.log(`count равен ${count}`)    
+//     }
+
+//     if (count==1) {
+//         console.log(`внешний цикл выдает i равной ${i}`);
+//     }
+//     count=0;
+// }
